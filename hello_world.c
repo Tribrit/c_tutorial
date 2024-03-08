3863531a7cb8f402fb88fd36e9c3cbee8d3df099
Tribrit <trizerbritney@gmail.com>
2024-03-08 19:16:51 +0300

    cleaned up Helloworld

--- hello_world.c
+++ hello_world.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    
     /*my first programe in c */
     printf("Hello ,World! \n");
-    printf("am coming\n");
-    printf("I have done wrong but i want to be better");
 
     return 0;
 }

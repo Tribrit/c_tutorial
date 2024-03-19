972835e5f76d508fdc166c622fc6a5610f5928b9
Tribrit <trizerbritney@gmail.com>
2024-03-19 21:34:58 +0300

    char.c added

---
+++ char.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+char message[]="\nHello world 2\n";
+
+printf("%s",message);
+
+return 0;
+}
---
+++ same .c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    char a = 251;
+    
+    unsigned Char b = a;
+
+    printf("a=%c",a);
+
+    printf("\n b=%c",b);
+
+
+    if (a == b)
+
+      printf("\nSame");
+
+      else
+
+      printf("\nNotsame");
+
+      return 0;
+
+
+}
